53957d0a6d778a20c0e6d95b775fdc59c17e7a13
zaquel de souza <zaquelsousa2013@hotmail.com>
2024-10-01 02:25:50 -0300

    fazendo jogo de tiro ao alvo com ts pq sim uai

---
+++ Queue/index.js
@@ -0,0 +1,18 @@
+"use strict";
+const canvas = document.getElementById("CanvasArea"); //kkkk confia ts
+if (canvas) {
+    const ctx = canvas.getContext("2d");
+    if (ctx) {
+        function Gameloop() {
+            //limpa o canvas
+            ctx === null || ctx === void 0 ? void 0 : ctx.clearRect(0, 0, canvas.width, canvas.height);
+            //func pra atualizar a logica vai vim aqui ou nao kkkkkkk
+            //fun pra desenhar os treco na tela
+            requestAnimationFrame(Gameloop);
+        }
+        Gameloop();
+    }
+}
+else {
+    console.error('Canvas não encontrado!');
+}
